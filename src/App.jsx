6d564be9c20556fa4d87aeba49f8bfbfcf8978be
anjali giri <girianjali1807@gmail.com>
2024-09-12 09:57:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import {About, Hero, Navbar , Tech , Works , Experience , Feedbacks , Contact} from './components/index'
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat  bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
+        
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
